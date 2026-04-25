33db41fef666647e7d0f979fbf3592c762f3a4b5
Nandini <nandunandinigoyal@gmail.com>
2026-04-25 14:03:02 +0530

    new

--- gesture.py
+++ gesture.py
@@ -1,3 +1,11 @@
+import os
+# Force xcb platform to fix blank/invisible OpenCV windows on Wayland
+if os.environ.get("XDG_SESSION_TYPE") == "wayland":
+    os.environ["QT_QPA_PLATFORM"] = "xcb"
+
+# Use external webcam (index 2 corresponds to /dev/video2)
+os.environ["CAMERA_INDEX"] = "2"
+
 from iot_control.gesture import main
 
 
